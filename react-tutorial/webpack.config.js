b7e3356cea4b72b2f3feead531076cfbadfa5ca9
mkw111 <mkw111@iener.net>
2024-11-30 09:16:58 +0900

    깃허브 코드 추가

--- react-tutorial/webpack.config.js
+++ react-tutorial/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     name: 'word-relay-setting',
     mode: 'development', //실서비스 : production
-    devtool: 'eval',
+    devtool: 'eval', //개발시에는 eval, 운영시에는 hidden-source-map
     resolve: {
         extensions: ['.js', '.jsx']
     }, //이걸 통해서 이름에 해당하는 확장자가 있는지 리졸버가 알아서 찾는다, 밑에꺼랑 연동이라 보면됨 client.js, client.jsx를 찾게됨
@@ -15,11 +15,11 @@ module.exports = {
 
     module: {
         rules: [{
-            test: /\.jsx?/,
+            test: /\.jsx?/, //js and jsx 파일에 바벨을 적용해서 최신 문법을 옛날 브라우저에서도 돌아가는 문법으로 변환
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'], //나의 경우엔 이게 없어도 되는거 같음
+                presets: ['@babel/preset-env', '@babel/preset-react'], //바벨 적용
+                plugins: ['@babel/plugin-proposal-class-properties'], //나의 경우엔 이게 없어도 되는거 같음, 플러그인[state쓰려면]
             }
         }],
     },
